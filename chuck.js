1153037fc7b68232a789cda407f8626d05a3a43e
bstk0 <rodrigo.bisterco@gmail.com>
2020-03-12 18:41:08 +0000

    test

--- chuck.js
+++ chuck.js
@@ -7,7 +7,9 @@ exports.getCall = function(request, response) {
   https.get("https://api.chucknorris.io/jokes/random" , function(res) {
 
     res.on("data", function(d) {
+      //JSON.parse(data).value
       response.json(res.statusCode, JSON.parse(d.toString()));
+      //response.json(res.statusCode, JSON.parse(d).value);
     });
 
     res.on("error", function(e) {
